89a6809e41d98e6fc64cebc0bd687e6ef46c82f5
Sergio Felix <sergiofelix9@gmail.com>
2023-09-14 14:21:32 -0300

    fix: finished the final ajusts

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Desenvolvedor FullStack",
+    "Desenvolvedor JavaScript",
+    "Desenvolvedor Web",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
